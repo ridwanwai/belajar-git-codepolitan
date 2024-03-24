d153644a907ad2f9254843e214febbde0c9472b4
Ridwan <ridwanbaniah@gmail.com>
2024-03-24 14:56:30 +0700

    memperbaiki fungsi kurang dan kali

--- fungsi.js
+++ fungsi.js
@@ -3,8 +3,8 @@ console.log("Belajar-git");
 const add = (a,b)=> a+b;
 console.log(add(1,5));
 
-const substruck = (a,b)=> a-b;
-console.log(5,2);
+const substruckNumber = (a,b)=> a-b;
+console.log(substruckNumber(5,2));
 
 const multiply = (a,b)=> a*b;
-console.log(4,6);
+console.log(multiply(4,6));
